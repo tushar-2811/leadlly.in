5be1ae75a9a2d1dc940bed32fdb0fe8470e56cf6
Davinder1436 <davindersingh1436@gmail.com>
2024-04-01 19:06:05 +0530

    commit

--- src/Pages/Sections/Testimonials/Testimonial.jsx
+++ src/Pages/Sections/Testimonials/Testimonial.jsx
@@ -14,19 +14,22 @@ const ComponentName = () => {
         <motion.div
         initial={{y:"80%" ,opacity:0}}
         whileInView={{y:"0% ",opacity:1}}
-        transition={{duration:0.5}}
+        transition={{duration:0.5 ,ease:"easeInOut"}}
 
          className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus?</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>
+            Mahip bhaiya is a very good mentor, the way in which he guided me and the tasks that he gave me are very good. I saw improvement in my learning skills. Also tracker and error book ne bhi meri bahut help ki. Overall leadlly helped me a lot in my journey....
+            </h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </motion.div>
         <motion.div
         initial={{y:"80%" ,opacity:0}}
         whileInView={{y:"0% ",opacity:1}}
-        transition={{duration:0.5}}
+        transition={{duration:0.5,ease:"easeInOut"}}
 
          className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus?</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>My mentor was Manan Bhaiya, He is a very good guide. He use to talk in a friendly manner( never felt that I am talking to an elder).He use to motivate me even in bad situations( like getting low grades..).
+After all, under his guidance I have seen good changes in my schedule and in myself ...</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </motion.div>
        </div>
@@ -37,10 +40,10 @@ const ComponentName = () => {
        <motion.div
         initial={{y:"80%" ,opacity:0}}
         whileInView={{y:"0% ",opacity:1}}
-        transition={{duration:0.5}}
+        transition={{duration:0.5,ease:"easeInOut"}}
 
          className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum ducimus quam provident repellendus est itaque quisquam suscipit porro eligendi praesentium. Totam doloribus perspici</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Talking about revision tracker and planners both these things helped me a lot and nearly stopped all my procrastination and anxiousness. And my mentor was Saumya didi. She is really helpful and relatable. She never disappoint me in her responses and I've tried doing what she told me to do and it has helped significantly in my process of learning better.</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </motion.div>
         </div>
@@ -49,19 +52,19 @@ const ComponentName = () => {
        <motion.div
         initial={{y:"80%" ,opacity:0}}
         whileInView={{y:"0% ",opacity:1}}
-        transition={{duration:0.5}}
+        transition={{duration:0.5,ease:"easeInOut"}}
 
          className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus?</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>This mentorship program is very helpful to me .Techniques that are discussed during workshops were really helpful for me and also my mentor helped me to implement those techniques in my daily routine which helps me a lot. My mentor also provides me lot of study material and motivates me. I really appreciate this program.</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </motion.div>
         <motion.div
         initial={{y:"80%" ,opacity:0}}
         whileInView={{y:"0% ",opacity:1}}
-        transition={{duration:0.5}}
+        transition={{duration:0.5,ease:"easeInOut"}}
 
          className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus?</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Leadlly seriously rocks! Their tips, support, and all that expert knowledge totally helped me level up. Thanks for being there every step of the way</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </motion.div></div>
        </div>
@@ -85,23 +88,24 @@ const ComponentName = () => {
        initial={{x:"-50%" ,opacity:0}}
        whileInView={{x:"0% ",opacity:1}}
        
-       transition={{duration:0.5}}>
+       transition={{duration:0.5,ease:"easeInOut" }}>
         <div className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus?</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1 '>Mahip bhaiya is a very good mentor, the way in which he guided me and the tasks that he gave me are very good. I saw improvement in my learning skills. Also tracker and error book ne bhi meri bahut help ki. Overall leadlly helped me a lot in my journey....</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </div>
         <div className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus?</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>My mentor was Manan Bhaiya, He is a very good guide. He use to talk in a friendly manner( never felt that I am talking to an elder).He use to motivate me even in bad situations( like getting low grades..).
+After all, under his guidance I have seen good changes in my schedule and in myself ...</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </div>
        </motion.div>
        <motion.div className='flex flex-col justify-center '
-       initial={{y:"100%" ,opacity:0}}
+       initial={{y:"50%" ,opacity:0}}
        whileInView={{y:"0% ",opacity:1}}
        
-       transition={{duration:0.5}}>
+       transition={{duration:0.5,ease:"easeInOut"}}>
        <div className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum ducimus quam provident repellendus est itaque quisquam suscipit porro eligendi praesentium. Totam doloribus perspici</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Talking about revision tracker and planners both these things helped me a lot and nearly stopped all my procrastination and anxiousness. And my mentor was Saumya didi. She is really helpful and relatable. She never disappoint me in her responses and I've tried doing what she told me to do and it has helped significantly in my process of learning better.</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </div>
         </motion.div>
@@ -109,13 +113,13 @@ const ComponentName = () => {
        initial={{x:"50%" ,opacity:0}}
        whileInView={{x:"0% ",opacity:1}}
        
-       transition={{duration:0.5}}>
+       transition={{duration:0.5,ease:"easeInOut"}}>
        <div className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus?</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>This mentorship program is very helpful to me .Techniques that are discussed during workshops were really helpful for me and also my mentor helped me to implement those techniques in my daily routine which helps me a lot. My mentor also provides me lot of study material and motivates me. I really appreciate this program.</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </div>
         <div className='ring-2 ring-pink-50 rounded-b-2xl rounded-t-md md:rounded-2xl ring-offset-2 shadow-xl shadow-purple-300 w-[90vw] p-5 m-5 md:w-[25vw]'>
-            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, cupiditate animi! Tenetur consectetur at asperiores reiciendis tempore aliquam in fuga ullam. Aut eveniet sit ratione atque optio ea aliquid ducimus?</h3>
+            <h3 className='text-[1rem] text-indigo-950 font-sans p-1'>Leadlly seriously rocks! Their tips, support, and all that expert knowledge totally helped me level up. Thanks for being there every step of the way</h3>
             <p className=' text-[0.8rem] indent-[50%] font-sans p-1'>~ davinder singh</p>
         </div></motion.div>
        </div>
--- src/Pages/Sections/WhyUs/WhyUs.jsx
+++ src/Pages/Sections/WhyUs/WhyUs.jsx
@@ -7,10 +7,10 @@ export default function WhyUs() {
 
       {/* for phone */}
       <motion.div 
-      initial={{ opacity: 0,x:"100%" }}
-      whileInView={{ opacity: 1, x:"0" }}
+      initial={{ opacity: 0, scale:0.5 }}
+      whileInView={{ opacity: 1,scale:1 }}
       transition={{ duration: 0.8,ease:"easeInOut" }}
-       className='block md:hidden bg-gradient-to-r bg-clip-text from-[#5900d9] - bg-purple-400  justify-start  ' ><h2 className='font-bold ml-5 text-[3rem] mt-10  text-transparent ' > Don't Stress!</h2></motion.div>
+       className='block md:hidden bg-gradient-to-r bg-clip-text from-[#5900d9] - bg-purple-400  justify-start  ' ><h2 className='font-bold md:hidden ml-5 text-[3rem] mt-10  text-transparent ' > Don't Stress!</h2></motion.div>
       <div className=' md:hidden grid grid-cols-12 grid-rows-12 w-[95vw] h-[100vh]  mx-auto my-5 gap-y-4 gap-x-4'>
         
         <motion.div initial={{ opacity: 0, scale: 0.5 }}
@@ -74,16 +74,16 @@ export default function WhyUs() {
       {/* for computer */}
       
       <motion.div 
-      initial={{ opacity: 0,x:"50%" }}
-      whileInView={{ opacity: 1, x:"0" }}
-      transition={{ duration: 0.8,ease:"easeInOut" }}
+      initial={{ opacity: 0,scale:0.5 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5,ease:"easeInOut" }}
        className='md:block hidden bg-gradient-to-r bg-clip-text from-[#5900d9] - bg-purple-400  ' ><h2 className='font-bold mx-[10%] text-[5rem] mt-10  text-transparent ' > Don't Stress!</h2></motion.div>
         <div className='hidden md:grid md:grid-cols-12 md:grid-rows-12 md:w-[80vw] md:h-[90vh] mx-auto mb-10 gap-y-4 gap-x-4'>
         
         <motion.div initial={{ opacity: 0, scale: 0.5 }}
         whileInView={{ opacity: 1, scale: 1 }}
        
-        transition={{ duration: 0.5 }} className='   md:col-span-3  md:row-span-5'><p className='text-[1.5rem] font-sans font-black mt-[20%] leading-tight'>we got your back with leadly's powerful learning suite:</p></motion.div>
+        transition={{ duration: 0.5 }} className='   md:col-span-3  md:row-span-4'><p className='text-[1.8rem] font-sans font-black  leading-tight'>we got your back with leadly's powerful learning suite:</p></motion.div>
         <div className='col-span-3  row-span-1'></div>
         
         
@@ -103,7 +103,7 @@ export default function WhyUs() {
         <motion.div initial={{ opacity: 0, scale: 0.5 }}
         whileInView={{ opacity: 1, scale: 1 }}
         
-        transition={{ duration: 0.5 }}  className='relative md:col-span-3  bg-[#faf8fe] md:row-span-2 rounded-2xl ring-2 ring-purple-100 ring-offset-2'>
+        transition={{ duration: 0.5 }}  className='relative md:col-span-3  bg-[#faf8fe] md:row-span-3 rounded-2xl ring-2 ring-purple-100 ring-offset-2'>
           <h2 className='text-[2.2vw] mt-[7%] font-mono ml-5 leading-tight font-semibold font-sans' >Workshops</h2><p></p>
         <img src="./imgs/sections/Group 165 1.png" className='absolute w-[25%] right-[1.5vw] bottom-0   h-auto '></img> </motion.div>
         
@@ -119,8 +119,8 @@ export default function WhyUs() {
         whileInView={{ opacity: 1, scale: 1 }}
         
         transition={{ duration: 1 }} className='relative md:col-span-3 bg-[#faf8fe] md:row-span-6 my-10 rounded-2xl ring-2 ring-purple-100 ring-offset-2'>
-          <h2 className='text-[3vw] mt-[50%] font-mono ml-5 leading-tight font-semibold font-sans' > Error Book </h2><p className='px-4'>embrace your mistake</p>
-          <img src="./imgs/sections/Group 163.png" className='absolute w-[60%] right-0 top-0  h-auto '></img> </motion.div>
+          <h2 className='text-[3vw] mt-[40%] font-mono ml-5 leading-tight font-semibold font-sans' > Error Book </h2><p className='px-4'>embrace your mistake</p>
+          <img src="./imgs/sections/Group 163.png" className='absolute w-[50%] right-0 top-0  h-auto '></img> </motion.div>
         <motion.div initial={{ opacity: 0, scale: 0.5 }}
         whileInView={{ opacity: 1, scale: 1 }}
         
